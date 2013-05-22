1d461d187337206bae28b8ead022acd056d9729c
Stephen Sugden <glurgle@gmail.com>
2013-05-22 16:50:07 -0700

    Make it testling-ci compatible

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var assert = require('assert')
+var test = require('tape')
 var pctEncode = require('./')
 
-var encode = pctEncode(/[^\w~.\-]/g);
+test('Does it work?', function (t) {
+  t.plan(1)
+  var encode = pctEncode(/[^\w~.\-]/g);
 
-assert.equal(encode("UTF-8 in your URIs: ✓"),
-             "UTF-8%20in%20your%20URIs%3A%20%E2%9C%93");
-
-console.log('ok');
+  t.equal(encode("UTF-8 in your URIs: ✓"),
+               "UTF-8%20in%20your%20URIs%3A%20%E2%9C%93");
+})
