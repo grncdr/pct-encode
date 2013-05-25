573a7f066e67ae1eeaaceedba413d3c8878ebac1
Stephen Sugden <glurgle@gmail.com>
2013-05-25 00:01:31 -0700

    Reduce number of browsers

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ test('Does it work?', function (t) {
   t.plan(1)
   var encode = pctEncode(/[^\w~.\-]/g);
 
-  t.equal(encode("UTF-8 in your URIs: ✓"),
-               "UTF-8%20in%20your%20URIs%3A%20%E2%9C%93");
+  t.equal(encode("UTF-8 in your URIs: ✓✓"),
+                 "UTF-8%20in%20your%20URIs%3A%20%E2%9C%93%E2%9C%93");
 })
